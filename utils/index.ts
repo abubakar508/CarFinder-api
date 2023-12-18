97fe7d6257973b1741b9a35e83acf6da7a809e51
Ismail M Abubakar <121245651+abubakar508@users.noreply.github.com>
2023-12-19 01:24:05 +0300

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ export async function fetchCars() {
         'X-RapidAPI-Key': 'fcacf29d42mshd2713384ca374e4p19e3c6jsnb1db951ad614',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
-      const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla&limit=30', {headers: headers, });
+      const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera&limit=30', {headers: headers, });
 
       const results = await response.json();
 
